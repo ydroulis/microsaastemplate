66d4c1d53a86078ea2142b275f192ae51ee4cc0e
ydroulis <lombardi.droulis@gmail.com>
2025-04-20 21:22:17 -0300

    fix error build

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes"
 
 export function handleMercadoPagoPayment(paymentData: PaymentResponse) {
